c963c83e4d817e108e7e96ecb58ee451fff80d8d
acid87 <jmnazarian@gmail.com>
2018-06-12 13:37:37 -0300

    Erase all files

--- example/comp_test.bat
+++
@@ -1,4 +0,0 @@
-avr-g++ -Wall -Os -mmcu=atmega328p -c -o ..\One-Weird.o ..\One-Weird.cpp
-avr-g++ -Wall -Os -mmcu=atmega328p -c -o test_lib.o test_lib.c
-avr-g++ -mmcu=atmega328p test_lib.o ..\One-Weird.o -o test_lib
-avr-objcopy -O ihex -R .eeprom test_lib test_lib.hex
