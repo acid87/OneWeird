8fc9081a8100a42bd5979c8d1e331179e15f0b69
acid87 <jmnazarian@gmail.com>
2018-06-13 14:42:46 -0300

    First commit

---
+++ One-Weird.h
@@ -0,0 +1,24 @@
+// One Wire protocol implementation by 4c1d.
+//
+
+#ifndef OneWeird_h
+	#define OneWeird_h
+	 
+	 
+	#define SENSOR_PORT PB2
+	#define LED_PORT PB1
+	#define SAMPLE_PORT PB3
+
+	typedef enum { false, true } bool;
+	typedef enum { HIGH, LOW } bit_t;
+
+	bool oneWire_initilization(void);
+	bool oneWire_write_slot(const unsigned char bit);
+	bit_t oneWire_read_slot(void);
+	bool oneWire_write_command(const unsigned char command);
+
+
+
+
+
+#endif
---
+++ example/test_lib.c
@@ -0,0 +1,107 @@
+
+
+//#define F_CPU 16000000 
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+#include "..\One-Weird.h"
+
+int i;
+
+int main(void)
+{
+    DDRB |= (1 << SENSOR_PORT) | (1 << LED_PORT) | (1 << SAMPLE_PORT); 
+    PORTB &= ~(1 << SAMPLE_PORT);
+
+    for(;;)
+    {
+        _delay_ms(100);        
+        //PORTB |= (1 << LED_PORT);
+        //   // toggle PD5
+        // _delay_ms(250);  // delay in ms, valor maximo = 260/F_CPU in MHz
+        // PORTB &= ~(1 << LED_PORT);
+        // _delay_ms(250);
+
+        // // test init
+        // if(oneWire_initilization() == true)
+        //  PORTB |= (1 << LED_PORT);
+        
+        // _delay_ms(500);        
+  //       PORTB |= (1 << LED_PORT);
+  //       // test write slot
+  //       oneWire_write_slot(0);
+  //       _delay_us(1);
+
+  //       oneWire_write_slot(1);
+  //       _delay_us(1);
+
+  //       oneWire_write_slot(1);
+  //       _delay_us(1);
+
+  //       PORTB &= ~(1 << LED_PORT);
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+        oneWire_initilization();
+        oneWire_write_command(0xCC);
+        oneWire_write_command(0x44);        
+        _delay_ms(500);
+        _delay_ms(500);
+
+        oneWire_initilization();
+        oneWire_write_command(0xCC);
+        PORTB |= (1 << LED_PORT);
+        oneWire_write_command(0xBE);
+        PORTB &= ~(1 << LED_PORT);
+
+        //oneWire_write_command(0xBE);
+        //oneWire_write_slot(1);
+        //oneWire_write_slot(0);
+        //_delay_us(1);
+        //oneWire_write_slot(0);
+
+        
+        _delay_us(10);
+        PORTB |= (1 << LED_PORT);   
+        for(i = 0; i < 16; i++ )
+        {
+            oneWire_read_slot();
+            _delay_us(1);
+        }
+        
+        PORTB &= ~(1 << LED_PORT);
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        // _delay_us(1);
+        // oneWire_read_slot();
+        // _delay_us(1);
+        // oneWire_read_slot();
+        // _delay_us(1);
+        // oneWire_read_slot();
+        // _delay_us(1);
+        // oneWire_read_slot();
+        // _delay_us(1);
+        // oneWire_read_slot();
+        // _delay_us(1);
+
+  //    oneWire_initilization();
+        // oneWire_write_command(0x33);
+         
+        // PORTB |= (1 << LED_PORT);    
+  //    for(i = 0; i < 16; i++ )
+  //    {
+  //        oneWire_read_slot();
+  //        _delay_us(1);
+  //    } 
+  //    PORTB &= ~(1 << LED_PORT);
+  //    _delay_ms(1);   
+                
+
+
+
+    }
+
+    return 0;  // the program executed successfully
+}
