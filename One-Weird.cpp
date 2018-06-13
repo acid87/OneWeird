d10529b9549b2f78759e1ed6619a4c07f5b7a293
acid87 <jmnazarian@gmail.com>
2018-06-13 14:49:25 -0300

    Change extension of One-Weird.c to .cpp and the return type of functions

--- One-Weird.cpp
+++ One-Weird.cpp
@@ -19,11 +19,11 @@
 
 
 // Funcion de inicializacion.
-// Obtenemos true si el sensor responde con la señal de presencia
-// false en caso contrario
-bool oneWire_initilization(void)
+// Obtenemos 1 si el sensor responde con la señal de presencia
+// 0 en caso contrario
+unsigned char oneWire_initilization(void)
 {
-	bool presence;
+	unsigned char presence;
 
 	PORTB &= ~(1 << SENSOR_PORT); // low signal (x0) tri state or output low
 	DDRB |= (1 << SENSOR_PORT);   // (10)
@@ -32,7 +32,7 @@ bool oneWire_initilization(void)
 	DDRB &= ~(1 << SENSOR_PORT);  // (00)
 	_delay_us(70); // the sensor waits between 15-60 µs from the rising edge and transmit a low for 60 to 240 µs. So i have to sample 60 - 75 µs.
 
-	presence = (( (PINB & (1 << SENSOR_PORT)) < 1) ? (true) : (false));
+	presence = (( (PINB & (1 << SENSOR_PORT)) < 1) ? (1) : (0));
 
 	_delay_us(300); // 60 (max time to wait) + 240 (max presence width)  
 
@@ -41,7 +41,7 @@ bool oneWire_initilization(void)
 
 // writes 0 if bit is equal to 0, writes 1 if bit is other value
 // 60 µs should be the minimum duration of write slot with 1 µs of recovery between write slots
-bool oneWire_write_slot(const unsigned char bit)
+void oneWire_write_slot(const unsigned char bit)
 {	
 
 	PORTB |= (1 << SENSOR_PORT);  // high signal (x1) // pull-up enable or output high
@@ -64,14 +64,13 @@ bool oneWire_write_slot(const unsigned char bit)
 
 	PORTB |= (1 << SENSOR_PORT);  // (11) output high
 	_delay_us(2);
-	return true;
 
 }
 
 // 60 µs should be the minimum duration of read slot with 1 µs of recovery between read slots
-bit_t oneWire_read_slot(void)
+unsigned char oneWire_read_slot(void)
 {
-	bit_t bit;
+	unsigned char bit;
 	PORTB &= ~(1 << SAMPLE_PORT);  // high sample port to indicate start of sampling
 
 	PORTB |= (1 << SENSOR_PORT);  // high signal (x1) // pull-up enable or output high
@@ -85,7 +84,7 @@ bit_t oneWire_read_slot(void)
 	_delay_us(8); 	// sample before 15 µs after the read slot starts
 	
 	PORTB |= (1 << SAMPLE_PORT);
-	bit = (( (PINB & (1 << SENSOR_PORT)) > 0) ? (HIGH) : (LOW));
+	bit = (( (PINB & (1 << SENSOR_PORT)) > 0) ? (1) : (0));
 
 	PORTB &= ~(1 << SAMPLE_PORT); // low sample port to indicate start of sampling
 
@@ -98,7 +97,7 @@ bit_t oneWire_read_slot(void)
 }
 
 // writes 1 byte command ie 0x33
-bool oneWire_write_command(const unsigned char command)
+void oneWire_write_command(const unsigned char command)
 {
 	int mask = 0;	
 
@@ -108,13 +107,11 @@ bool oneWire_write_command(const unsigned char command)
 		_delay_us(2);
 	}
 
-	return true;
 }
 
 // writes 1 byte command ie 0x33
-bool oneWire_read_byte(void)
+void oneWire_read_byte(void)
 {
 
 
-	return true;
 }
--- One-Weird.h
+++ One-Weird.h
@@ -4,18 +4,16 @@
 #ifndef OneWeird_h
 	#define OneWeird_h
 	 
-	 
+
 	#define SENSOR_PORT PB2
 	#define LED_PORT PB1
 	#define SAMPLE_PORT PB3
 
-	typedef enum { false, true } bool;
-	typedef enum { HIGH, LOW } bit_t;
 
-	bool oneWire_initilization(void);
-	bool oneWire_write_slot(const unsigned char bit);
-	bit_t oneWire_read_slot(void);
-	bool oneWire_write_command(const unsigned char command);
+	unsigned char oneWire_initilization(void);
+	void oneWire_write_slot(const unsigned char bit);
+	unsigned char oneWire_read_slot(void);
+	void oneWire_write_command(const unsigned char command);
 
 
 
